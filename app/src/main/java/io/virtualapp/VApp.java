9bced2fcece8e97a92e9fec1d5d430883ea3ea33
zhangguangjin <zhangguangjin@xiaomi.com>
2019-03-06 09:59:26 +0800

    去掉蓝牙模式，去掉Android p 调用隐藏API的弹窗
    Signed-off-by: zhangguangjin <zhangguangjin@xiaomi.com>

--- app/src/main/java/io/virtualapp/VApp.java
+++ app/src/main/java/io/virtualapp/VApp.java
@@ -11,6 +11,9 @@ import com.lody.virtual.client.core.VirtualCore;
 import com.lody.virtual.client.stub.VASettings;
 import com.lody.virtual.helper.utils.Reflect;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import io.virtualapp.delegate.MyAppRequestListener;
 import io.virtualapp.delegate.MyComponentDelegate;
 import io.virtualapp.delegate.MyPhoneInfoDelegate;
@@ -38,6 +41,32 @@ public class VApp extends MultiDexApplication {
         try {
             VirtualCore.get().startup(base);
         } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        if(android.os.Build.VERSION.SDK_INT >= 28/*android.os.Build.VERSION_CODES.P*/){
+            closeAndroidPDialog();
+        }
+    }
+
+
+    private void closeAndroidPDialog(){
+//        try {
+//            Class aClass = Class.forName("android.content.pm.PackageParser$Package");
+//            Constructor declaredConstructor = aClass.getDeclaredConstructor(String.class);
+//            declaredConstructor.setAccessible(true);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        try {
+            Class cls = Class.forName("android.app.ActivityThread");
+            Method declaredMethod = cls.getDeclaredMethod("currentActivityThread");
+            declaredMethod.setAccessible(true);
+            Object activityThread = declaredMethod.invoke(null);
+            Field mHiddenApiWarningShown = cls.getDeclaredField("mHiddenApiWarningShown");
+            mHiddenApiWarningShown.setAccessible(true);
+            mHiddenApiWarningShown.setBoolean(activityThread, true);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
